f2269e924a9490e7f8ee0fd3361373243c53b0dc
PedroCD <pedro@buyviu.com>
2023-08-16 12:01:04 +0100

    Simplify navigation. Add dynamic rendering due to refactor

--- src/router/index.js
+++ src/router/index.js
@@ -8,27 +8,7 @@ const routes = [
         component: Home
     },
     {
-        path: '/brazil',
-        name: 'Brazil',
-        component: () => import(/* webpackChunkName: "brazil" */"../views/Brazil.vue")
-    },
-    {
-        path: '/hawaii',
-        name: 'Hawaii',
-        component: () => import(/* webpackChunkName: "hawaii" */"../views/Hawaii.vue")
-    },
-    {
-        path: '/jamaica',
-        name: 'Jamaica',
-        component: () => import(/* webpackChunkName: "jamaica" */"../views/Jamaica.vue")
-    },
-    {
-        path: '/panama',
-        name: 'Panama',
-        component: () => import(/* webpackChunkName: "panama" */"../views/Panama.vue")
-    },
-    {
-        path: '/destination/:id',
+        path: '/destination/:id/:slug',
         name: 'destination.show',
         component: () => import(/* webpackChunkName: "destinationShow" */"../views/DestinationShow.vue")
     }
