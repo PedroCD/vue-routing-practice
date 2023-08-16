c1052b8b6f6818a9448f0f0f08adcddb1ebdb041
PedroCD <pedro@buyviu.com>
2023-08-16 10:37:12 +0100

    Add dynamical routing using json and v-for

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-    <HelloWorld msg="Vite + Vue" />
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/brazil">Brazil</router-link>
+      <router-link to="/hawaii">Hawaii</router-link>
+      <router-link to="/jamaica">Jamaica</router-link>
+      <router-link to="/panama">Panama</router-link>
+    </div>
   </div>
-  <router-view/>
+  <div class="container">
+    <router-view/>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,26 @@ const routes = [
         path: '/about',
         name: 'About',
         component: () => import(/* webpackChunkName: "about" */"../views/About.vue")
+    },
+    {
+        path: '/brazil',
+        name: 'Brazil',
+        component: () => import(/* webpackChunkName: "brazil" */"../views/Brazil.vue")
+    },
+    {
+        path: '/hawaii',
+        name: 'Hawaii',
+        component: () => import(/* webpackChunkName: "hawaii" */"../views/Hawaii.vue")
+    },
+    {
+        path: '/jamaica',
+        name: 'Jamaica',
+        component: () => import(/* webpackChunkName: "jamaica" */"../views/Jamaica.vue")
+    },
+    {
+        path: '/panama',
+        name: 'Panama',
+        component: () => import(/* webpackChunkName: "panama" */"../views/Panama.vue")
     }
 ]
 
