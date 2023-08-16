39a6fc4997bfd8e323fd0cc5f6c60cd865d7905d
PedroCD <pedro@buyviu.com>
2023-08-16 09:41:17 +0100

    Starting routing

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Home from "../views/Home.vue";
+import {createRouter, createWebHistory} from "vue-router";
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: () => import(/* webpackChunkName: "about" */"../views/About.vue")
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router;
