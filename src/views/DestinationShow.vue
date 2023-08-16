9aeb1dbf9b8e4add8eae5b2d8830e2cd0c7421c0
PedroCD <pedro@buyviu.com>
2023-08-16 13:17:11 +0100

    Return to local json instead of fetch for exercises simplicity

--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -11,24 +11,13 @@
 <script>
 import sourceData from '../data.json'
 export default {
-  data(){
-    return {
-      destination: null,
-    }
-  },
   computed: {
     destinationId(){
       return parseInt(this.$route.params.id)
+    },
+    destination() {
+      return sourceData.destinations.find(destination => destination.id === this.destinationId)
     }
   },
-  methods: {
-    async initData(){
-      const response = await fetch(`https://travel-dummy-api.netlify.app/${this.$route.params.slug}.json`)
-      this.destination = await response.json()
-    }
-  },
-  async created(){
-    await this.initData()
-  }
 }
 </script>
