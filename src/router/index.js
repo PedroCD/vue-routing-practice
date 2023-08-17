df65d91c40672103de396819b773af246b636c34
PedroCD <pedro@buyviu.com>
2023-08-17 16:22:20 +0100

    Add scroll behavior

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Home from "../views/Home.vue";
 import {createRouter, createWebHistory} from "vue-router";
+import NotFound from "@/views/NotFound.vue";
+import sourceData from '@/data.json';
 
 const routes = [
     {
@@ -19,13 +21,33 @@ const routes = [
                 component: () => import(/* webpackChunkName: "experienceShow" */"../views/ExperienceShow.vue"),
                 props: (route) => ({...route.params, id: parseInt(route.params.id)})
             }
-        ]
+        ],
+        beforeEnter(to, from){
+            const exists = sourceData.destinations.find(destination => destination.id === parseInt(to.params.id))
+
+            if (!exists) return {
+                name: 'NotFound',
+                params: {pathMatch: to.path.split('/').slice(1)},
+                query: to.query,
+                hash: to.hash
+            }
+        },
     },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        component: () => import(/* webpackChunkName: "notFound" */"../views/NotFound.vue"),
+    }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+    scrollBehavior(to, from, savedPosition) {
+        return savedPosition || new Promise((resolve) => {
+            setTimeout(() => resolve({top: 0, behavior: 'smooth'}), 300)
+        })
+    }
 })
 
 export default router;
