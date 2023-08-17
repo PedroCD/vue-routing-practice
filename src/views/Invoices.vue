ff20ef67aeff3eb4b8bced24c54eea4d04809547
PedroCD <pedro@buyviu.com>
2023-08-17 17:35:47 +0100

    Playing with Composition API

--- src/views/Invoices.vue
+++ src/views/Invoices.vue
@@ -3,3 +3,15 @@
     <h1>Invoices</h1>
   </div>
 </template>
+
+<script>
+import {onBeforeRouteLeave} from 'vue-router'
+export default {
+  setup() {
+    onBeforeRouteLeave((to, from) => {
+      const answer = window.confirm("are you sure????")
+      if (!answer) return false;
+    })
+  }
+}
+</script>
